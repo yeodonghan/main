860b7089da06f23b0f34c4b201f9cdf6b382c345
yeodonghan <yeodonghan@gmail.com>
2019-10-08 13:38:15 +0800

    Fixed JsonAdaptedCustomer

--- src/main/java/seedu/address/storage/JsonAdaptedCustomer.java
+++ src/main/java/seedu/address/storage/JsonAdaptedCustomer.java
@@ -1,4 +1,92 @@
 package seedu.address.storage;
 
-public class JsonAdaptedCustomer {
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import seedu.address.commons.exceptions.IllegalValueException;
+import seedu.address.model.customer.ContactNumber;
+import seedu.address.model.customer.Customer;
+import seedu.address.model.customer.Email;
+import seedu.address.model.customer.Name;
+import seedu.address.model.tag.Tag;
+
+/**
+ * Jackson-friendly version of {@link Customer}.
+ */
+class JsonAdaptedCustomer {
+
+    public static final String MISSING_FIELD_MESSAGE_FORMAT = "Customer's %s field is missing!";
+
+    private final Name name;
+    private final ContactNumber contactNumber;
+    private final Email email;
+    private final List<JsonAdaptedTag> tagged = new ArrayList<>();
+
+    /**
+     * Constructs a {@code JsonAdaptedCustomer} with the given person details.
+     */
+    @JsonCreator
+    public JsonAdaptedCustomer(@JsonProperty("name") Name name,
+                             @JsonProperty("contactNumber") ContactNumber contactNumber,
+                             @JsonProperty("email") Email email,
+                             @JsonProperty("tagged") List<JsonAdaptedTag> tagged) {
+        this.name = name;
+        this.contactNumber = contactNumber;
+        this.email = email;
+        if (tagged != null) {
+            this.tagged.addAll(tagged);
+        }
+    }
+
+    /**
+     * Converts a given {@code Customer} into this class for Jackson use.
+     */
+    public JsonAdaptedCustomer(Customer source) {
+        name = source.getName();
+        contactNumber = source.getContactNumber();
+        email = source.getEmail();
+        tagged.addAll(source.getTags().stream()
+                .map(JsonAdaptedTag::new)
+                .collect(Collectors.toList()));
+    }
+
+    /**
+     * Converts this Jackson-friendly adapted person object into the model's {@code Customer} object.
+     *
+     * @throws IllegalValueException if there were any data constraints violated in the adapted Customer.
+     */
+    public Customer toModelType() throws IllegalValueException {
+        final List<Tag> customerTags = new ArrayList<>();
+        for (JsonAdaptedTag tag : tagged) {
+            customerTags.add(tag.toModelType());
+        }
+
+        if (name == null) {
+            throw new IllegalValueException(String.format(MISSING_FIELD_MESSAGE_FORMAT, Name.class.getSimpleName()));
+        }
+        if (!Name.isValidName(name.toString())) {
+            throw new IllegalValueException(Name.MESSAGE_CONSTRAINTS);
+        }
+        final Name modelName = new Name(name.toString());
+
+        final ContactNumber modelContactNumber = new ContactNumber(contactNumber.toString());
+
+        if (email == null) {
+            throw new IllegalValueException(String.format(MISSING_FIELD_MESSAGE_FORMAT, Email.class.getSimpleName()));
+        }
+        if (!Email.isValidEmail(email.toString())) {
+            throw new IllegalValueException(Email.MESSAGE_CONSTRAINTS);
+        }
+        final Email modelEmail = new Email(email.toString());
+
+        final Set<Tag> modelTags = new HashSet<>(customerTags);
+        return new Customer(modelName, modelContactNumber, modelEmail, modelTags);
+    }
+
 }
